ad5ad30e078a1ae945bbd837b1ffa14fb97fda45
Johnnys <uribe10201709@gmail.com>
2023-10-15 11:38:16 -0500

    creacion de evento aleta en js

---
+++ Js/main.js
@@ -0,0 +1,11 @@
+"use strict"
+let btn_IniciarSeion = document.getElementById("btn__log-in");
+
+
+ function alertaInicio(){
+    alert("¿Desas iniciar sesion?");
+ }
+
+ btn_IniciarSeion.addEventListener('click', ()=>{
+    alertaInicio();
+ })
